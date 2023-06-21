da1a7385da021064c4cd8e04e1c70b4990b2c9e4
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2023-06-22 00:36:58 +0530

    trying to hide the footer

--- Home.py
+++ Home.py
@@ -1,4 +1,9 @@
 import streamlit as st
+hide_footer_style = """
+    <style>
+    .reportview-container .main footer {visibility: hidden;}    
+    """
+st.markdown(hide_footer_style, unsafe_allow_html=True)
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
