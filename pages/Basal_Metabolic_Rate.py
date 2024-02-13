d26d7f4c1ab86942a67023a823770fd16637550d
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2024-02-13 10:52:13 +0530

    Update Basal_Metabolic_Rate.py

--- pages/Basal_Metabolic_Rate.py
+++ pages/Basal_Metabolic_Rate.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-# Basal Metabolic Rate (BMR) calculation
+# Basal Metabolic Rate (BMR) calculation using Mifflin-St Jeor Equation
 def calculate_bmr(weight, height, age, gender, metric):
     if metric == 'FPS (Foot Pound Second)':
         weight = weight * 0.45359237  # Convert pounds to kg
-        height = height * 0.3048  # Convert feet to meters
+        height = height * 30.48  # Convert feet to cm
     
     if gender == 'Male':
-        bmr = 4.184 * weight * height - 5 * age + 5
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
     else:
-        bmr = 4.184 * weight * height - 5 * age - 161
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
     return round(bmr, 2)
 
 # Streamlit UI
@@ -22,8 +22,8 @@ st.write("Enter your information to calculate your BMR.")
 metric_options = ['FPS (Foot Pound Second)', 'SI (International System of Units)']
 selected_metric = st.radio("Select Metric", metric_options)
 
-weight = st.number_input("Weight:")
-height = st.number_input("Height:")
+weight = st.number_input("Weight (kg if SI, lbs if FPS):")
+height = st.number_input("Height (cm if SI, ft if FPS):")
 age = st.number_input("Age:")
 gender = st.radio("Gender", ['Male', 'Female'])
 
